af918cc764346b4b40363e1de396b17e6392a8dc
desoulchild <33025335+dayosodimu@users.noreply.github.com>
2021-08-01 15:17:45 -0400

    Add project files.

---
+++ testing loops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testing_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
